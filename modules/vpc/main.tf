05193a38b8d4d04027f449e92443c0788ec0f318
kavya <vijayakavya1226@gmail.com>
2023-11-29 11:19:31 -0800

    expense shell

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -9,7 +9,7 @@ resource "aws_subnet" "public_subnets" {
   count = length(var.public_subnets)
   vpc_id = aws_vpc.main.id
   cidr_block = var.public_subnets[count.index]
-  availability_zone = var.public_subnets[count.index]
+  availability_zone = var.azs[count.index]
   tags = {
     Name = "public_subnet-${count.index}"
   }
@@ -20,7 +20,7 @@ resource "aws_subnet" "private_subnets" {
   count = length(var.private_subnets)
   vpc_id = aws_vpc.main.id
   cidr_block = var.private_subnets[count.index]
-  availability_zone = var.private_subnets[count.index]
+  availability_zone = var.azs[count.index]
   tags = {
     Name = "private_subnet-${count.index}"
   }
