65e71051e689f813354e2df119dec02850ce7d5e
kavya <vijayakavya1226@gmail.com>
2023-11-29 16:52:31 -0800

    expense shell

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -100,6 +100,20 @@ resource "aws_route" "default_route_table" {
 
 }
 
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnets)
+  subnet_id = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.public.id
+
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnets)
+  subnet_id = aws_subnet.private_subnets[count.index].id
+  route_table_id = aws_route_table.private.id
+
+}
+
 
 
 
