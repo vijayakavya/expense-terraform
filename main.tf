e80098cd2e8ec5aee4fdd8cd08dbae8b7095027f
kavya <vijayakavya1226@gmail.com>
2023-12-02 11:37:05 -0800

    expense shell

--- main.tf
+++ main.tf
@@ -45,4 +45,15 @@ module "frontend" {
   subnets = module.vpc.private_subnets
 }
 
+module "backend" {
+  source = "./modules/app"
+  app_port = 80
+  component = "backend"
+  env = var.env
+  instance_type = "t3.micro"
+  vpc_cidr = var.vpc_cidr
+  vpc_id = module.vpc.vpc_id
+  subnets = module.vpc.private_subnets
+}
+
 
