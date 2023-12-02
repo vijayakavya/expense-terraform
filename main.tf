11b3eea9275f93269629d3a410df44ffd5b3aefa
kavya <vijayakavya1226@gmail.com>
2023-12-02 10:52:54 -0800

    expense shell

--- main.tf
+++ main.tf
@@ -23,7 +23,15 @@ module"public-lb" {
   vpc_id            = module.vpc.vpc_id
 }
 
-
+module"private-lb" {
+  source = "./modules/alb"
+  alb_sg_allow_cidr = var.vpc_cidr
+  alb_type          = "private"
+  env               = var.env
+  internal          = true
+  subnets           = module.vpc.private_subnets
+  vpc_id            = module.vpc.vpc_id
+}
 
 
 
