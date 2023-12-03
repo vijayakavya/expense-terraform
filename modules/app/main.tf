4fb809a61d8ec301d478b33154a73a8297ec48ea
kavya <vijayakavya1226@gmail.com>
2023-12-02 18:03:54 -0800

    expense shell

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -34,6 +34,51 @@ resource "aws_security_group" "security_group" {
   }
 }
 
+resource "aws_iam_role" "role" {
+  name = "${var.env}-${var.component}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    name = "${var.env}-${var.component}-policy"
+
+    policy = jsonencode({
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Sid": "VisualEditor0",
+          "Effect": "Allow",
+          "Action": [
+            "ssm:DescribeParameters",
+            "ssm:GetParametersByPath",
+            "ssm:GetParameters"
+          ],
+          "Resource": "*"
+        }
+      ]
+
+    })
+
+
+  }
+
+
+  tags = {
+    tag-key = "${var.env}-${var.component}-role"
+  }
+}
 
 
 
