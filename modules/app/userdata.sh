f6cd79ceb3922f943005622aeb31c4aa6b2b1c62
kavya <vijayakavya1226@gmail.com>
2023-12-06 10:39:52 -0800

    expense shell

--- modules/app/userdata.sh
+++ modules/app/userdata.sh
@@ -2,4 +2,4 @@
 
 dnf install python3.11-pip -y | tee -a /opt/userdata.log
 pip3.11 install boto3 botocore | tee -a /opt/userdata.log
-ansible-pull -i localhost, -U https://github.com/kavyadevopsb26/infra-ansible main.yml -e role_name=${role_name} | tee -a /opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/vijayakavya/infra-ansible main.yml -e role_name=${role_name} | tee -a /opt/userdata.log
